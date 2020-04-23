cbf17e8a4a80bfaf77e8ac12a807efe4b6c2c1f6
James Kocher <JKocher13@yahoo.com>
2020-04-22 23:17:54 -0400

    added Limited consumer

---
+++ phase1/consumer-LimitConsumer-xactions.py
@@ -0,0 +1,62 @@
+from kafka import KafkaConsumer, TopicPartition
+from json import loads
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+class Transaction(Base):
+    __tablename__ = 'transaction'
+    id = Column(Integer, primary_key=True)
+    custid = Column(Integer)
+    type = Column(String(250), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
+
+class XactionConsumer:
+    def __init__(self):
+        self.consumer = KafkaConsumer('bank-customer-events',
+            bootstrap_servers=['localhost:9092'],
+            # auto_offset_reset='earliest',
+            value_deserializer=lambda m: loads(m.decode('ascii')))
+        ## These are two python dictionarys
+        # Ledger is the one where all the transaction get posted
+        self.ledger = {}
+        # custBalances is the one where the current blance of each customer
+        # account is kept.
+        self.custBalances = {}
+        # THE PROBLEM is every time we re-run the Consumer, ALL our customer
+        # data gets lost!
+        # add a way to connect to your database here.
+        self.engine = create_engine('mysql+pymysql://root:zipcoder@localhost/kafka_1')
+        self.limit = -5000
+
+
+
+
+        #Go back to the readme.
+
+    def handleMessages(self):
+        for message in self.consumer:
+            message = message.value
+            print('{} received'.format(message))
+            self.ledger[message['custid']] = message
+            message_sql = Transaction(custid=message['custid'], type=message['type'], date=message['date'], amt = message['amt'])
+            Session = sessionmaker(bind=self.engine)
+            session = Session()
+            session.add(message_sql)
+            session.commit()
+            if message['custid'] not in self.custBalances:
+                self.custBalances[message['custid']] = 0
+            if message['type'] == 'dep':
+                self.custBalances[message['custid']] += message['amt']
+            if message['type'] == 'wth' and self.custBalances[message['custid']] < self.limit:
+                print("insufficient funds")
+            else:
+                self.custBalances[message['custid']] -= message['amt']
+            print(self.custBalances)
+
+if __name__ == "__main__":
+    c = XactionConsumer()
+    c.handleMessages()
